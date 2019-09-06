b9d22a393304a0f368cad81e0f4e4938144d2fea
Eddy Tyannikov <etyannikov@gmail.com>
2019-09-06 18:05:25 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,8 @@
 def find_element_index(array, value_to_find)
-  array.length.times do
-    if 
+  array.length.times do |index|
+    if array[index] = value_to_find{
+      
+    }
 end
 
 def find_max_value(array)
